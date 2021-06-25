3097b9c2e07ec4464ccd60053b010f1b48d5c009
emilydowney <80785791+emilydowney@users.noreply.github.com>
2021-06-25 12:21:43 -0500

    Movie components added

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MainView } from './components/main-view/main-view';
 
+// Import statement to bundle ./index.scss
 import './index.scss';
 
+
+// Main component
 class MoviesApplication extends React.Component {
   render() {
     return (
-      <div className='my-flix'>
-        <div>Good morning</div>
-      </div>
+      <MainView />
     );
   }
 }
 
+// Finds the root of the app
 const container = document.getElementsByClassName('app-container')[0];
 
+// Tells React to render the app in the root DOM element
 ReactDOM.render(React.createElement(MoviesApplication), container);
