396d38ef177470de580bb576301e09468621383c
emilydowney <80785791+emilydowney@users.noreply.github.com>
2021-06-29 14:49:45 -0500

    Add registration view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -13,7 +13,16 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImageURL: PropTypes.string.isRequired
+    ImageURL: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired
+    }),
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    })
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+export function RegistrationView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
+
+  const handleSubmit = () => {
+    e.preventDefault();
+    console.log(username, password, email, birthday);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit
+      </button>
+    </form>
+  );
+}
