d4cbaea2a19cb1717014e8eee9caaa2b426ec5ee
emilydowney <80785791+emilydowney@users.noreply.github.com>
2021-07-23 15:00:23 -0500

    Fix delete user function and update UserView

--- src/components/user-view/user-view.jsx
+++ src/components/user-view/user-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Moment from 'moment';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
@@ -8,33 +9,70 @@ import axios from 'axios';
 
 export class UserView extends React.Component {
 
-  render() {
-    const { user, userData } = this.props;
+  constructor() {
+    super();
+    this.state = {
+      username: null,
+      password: null,
+      email: null,
+      birthday: null,
+      favoriteMovies: [],
+    };
+  }
 
-    // Function to delete user profile
-    function deleteUser() {
-      let token = localStorage.getItem('token');
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    this.getUser(accessToken);
+  }
 
-      axios.delete('https://a-movies-api.herokuapp.com/users/${user}', { headers: { Authorization: `Bearer ${token}` } })
-
-        .then(response => {
-          alert('Account was deleted.')
-        })
-        .catch(function (error) {
-          console.log(error);
+  getUser(token) {
+    let url = 'https://a-movies-api.herokuapp.com/users/' + localStorage.getItem('user');
+    axios.get(url, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => {
+        console.log(response.data)
+        this.setState({
+          username: response.data.Username,
+          password: response.data.Password,
+          email: response.data.Email,
+          birthday: Moment(response.data.Birthday).utc().format('ll'),
+          favoriteMovies: response.data.Favorites
         });
-    }
+      });
+  }
+
+  // Function to delete user profile
+  deleteUser() {
+    let url = 'https://a-movies-api.herokuapp.com/users/' + localStorage.getItem('user');
+    let token = localStorage.getItem('token');
+
+    axios.delete(url, { headers: { Authorization: `Bearer ${token}` } })
+
+      .then(response => {
+        alert('Account was deleted.');
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        window.location.reload(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
 
     return (
       <div className="container">
         <Row className="user-view">
           <Col md={11}>
             <h5>My Profile</h5>
-            <p>Username: {user}<br />
-              Email: {`${userData.Email}`}
+            <p>Username: {this.state.username}<br />
+              Email: {this.state.email}<br />
+              Birthday: {this.state.birthday}
             </p>
 
-            <Button variant="danger" onClick={() => { deleteUser() }}> Delete Account</Button>
+            <Button variant="danger" onClick={() => { this.deleteUser() }}> Delete Account</Button>
           </Col>
         </Row>
       </div>
